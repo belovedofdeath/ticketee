aa1e444243e38d7b85978cf37bfb34bf9d9474d2
Mel <melissamatara@gmail.com>
2012-04-11 10:52:42 -0700

    Implemented creating tickets for a project.

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,5 +1,6 @@
 class TicketsController < ApplicationController
 	before_filter :find_project
+	before_filter :find_ticket, :only => [:show, :edit, :update, :destroy]
 	def new
 		@ticket = @project.tickets.build
 	end
@@ -13,8 +14,23 @@ class TicketsController < ApplicationController
 			render :action => "new"
 		end
 	end
+	def show
+
+	end
+	def edit
+
+	end
+	def update
+
+	end
+	def destroy
+
+	end
 	private
 		def find_project
 			@project = Project.find(params[:project_id])
+		end
+		def find_ticket
+			@ticket = @project.tickets.find(params[:id])
 		end
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,6 @@
 class Ticket < ActiveRecord::Base
-  belongs_to :project
-  attr_accessible :description, :title
+	belongs_to :project
+	validates :title, :presence => true
+	validates :description, :presence => true, :length => { :minimum => 10 }
+	attr_accessible :description, :title
 end
